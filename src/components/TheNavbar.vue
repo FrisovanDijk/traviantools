0c710b12e81cf9b08fac7492e7419da7a2dce79b
Friso-private <mail@frisovandijk.com>
2023-01-02 23:02:12 +0100

    added Egyptians and Huns buildings

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="w-full flex bg-emerald-800 text-white p-3 flex">
-        <div>Friso's Travian Tools v2.0</div>
+        <div><span class="font-semibold">Friso's Travian Tools v2.1</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">now with 5 tribes buildings</span></div>
         <slot></slot>
     </div>
 </template>
