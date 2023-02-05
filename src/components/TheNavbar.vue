31d8eb520921df27406e83f4f9a33da7ce34a094
Friso-private <mail@frisovandijk.com>
2023-02-05 14:27:10 +0100

    app title

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="w-full flex bg-emerald-800 text-white p-3 flex">
-        <div><span class="font-semibold">Friso's Travian Tools v2.1</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">now with 5 tribes buildings</span></div>
+        <div><span class="font-semibold">Friso's Travian Tools v2.2</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">ROI calculator part 1</span></div>
         <slot></slot>
     </div>
 </template>
