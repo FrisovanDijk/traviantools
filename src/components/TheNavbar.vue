934415f92a74788e38afeb6d14f8e05b30a08ff2
Friso-private <mail@frisovandijk.com>
2023-02-19 23:27:33 +0100

    updated navbar to v2.3

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div class="w-full flex bg-emerald-800 text-white p-3 flex">
-        <div><span class="font-semibold">Friso's Travian Tools v2.2</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">ROI calculator part 1</span></div>
+        <div><span class="font-semibold">Friso's Travian Tools v2.3</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">ROI calculator with res building and HM ROI</span></div>
         <slot></slot>
     </div>
 </template>
