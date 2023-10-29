4a806a58a9be6c9ceb97d77c743ed27f9643a919
Friso-private <mail@frisovandijk.com>
2023-10-29 18:51:26 +0100

    updated 3.0 header

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -5,7 +5,9 @@
 <template>
     <div class="w-full flex text-white p-3 flex" style="background: rgb(5,150,105);
 background: linear-gradient(111deg, rgba(5,150,105,1) 33%, rgba(217,119,6,1) 33%, rgba(217,119,6,1) 70%, rgba(71,85,105,1) 70%);">
-        <div><span class="font-semibold">Friso's Travian Tools v3.0</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">Beta</span></div>
+        <div><span class="font-semibold">Friso's Travian Tools v3.0</span><span class="text-sm hidden sm:inline-block pl-4 text-gray-200">
+            New look, new tools, new version!
+        </span></div>
         <slot></slot>
     </div>
 </template>
