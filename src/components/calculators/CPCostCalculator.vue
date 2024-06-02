52696fe4d76e6a2ecc9d6200e4fcf5f36871fcc3
Friso-private <mail@frisovandijk.com>
2024-06-02 13:45:41 +0200

    finalised CP cost calculator with edit mode, sorting and failsafes

--- src/components/calculators/CPCostCalculator.vue
+++ src/components/calculators/CPCostCalculator.vue
@@ -12,6 +12,8 @@
 
     const calculator = userData.tabs[userData.currentTab].calculators[props.index].calculator
 
+    const tab = ref(0)
+
     onBeforeMount(() => {
         calculateCPCost()
     })
@@ -22,7 +24,7 @@
 
     const modifyLevel = (building, additive) => {
         if(additive) {
-            if(building.level < 20) {
+            if((building.name !== "Cranny" && building.level < 19) || building.level < 9) {
                 building.level++
                 calculateBuildingCPCost(building)
             }
@@ -38,49 +40,106 @@
 
     const calculateCPCost = () => {
         calculator.buildings.forEach((building) => {
-            calculateBuildingCPCost(building, false)
+            calculateBuildingCPCost(building)
         })
-
-        calculator.buildings.sort((a, b) => a.resCp - b.resCp)
     }
 
-    const calculateBuildingCPCost = (building, withSort = true) => {
+    const calculateBuildingCPCost = (building) => {
         const nextLevel = getResLevel(building.name, building.level)
-        const secondLevel = getResLevel(building.name, building.level + 1)
-        const thirdLevel = getResLevel(building.name, building.level + 2)
+        const nextCp = [nextLevel.cp]
+        const nextRes = [nextLevel.res]
+        const resCp = [Math.round(nextRes[0] / nextCp[0])]
 
-        const nextCp = [nextLevel.cp, nextLevel.cp + secondLevel.cp, nextLevel.cp + secondLevel.cp + thirdLevel.cp ]
-        const resCp = [
-            Math.round(nextLevel.res / nextLevel.cp),
-            Math.round((nextLevel.res + secondLevel.res) / nextCp[1]),
-            Math.round((nextLevel.res + secondLevel.res + thirdLevel.res) / nextCp[2])
-        ]
+        if((building.name !== "Cranny" && building.level + 1 < 20) || building.level + 1 < 10) {
+            const secondLevel = getResLevel(building.name, building.level + 1)
 
-        if(resCp[0] < resCp[1]) {
-            building.to = 0
+            nextCp.push(nextCp[0] + secondLevel.cp)
+            nextRes.push(nextRes[0] + secondLevel.res)
+            resCp.push(Math.round(nextRes[1] / nextCp[1]))
+        } else {
+            nextCp.push(nextCp[0])
+            nextRes.push(nextRes[0])
+            resCp.push(Math.round(nextRes[1] / nextCp[1]) + 1)
+        }
+
+        if((building.name !== "Cranny" && building.level + 2 < 20) || building.level + 2 < 10) {
+            const thirdLevel = getResLevel(building.name, building.level + 2)
+
+            nextCp.push(nextCp[1] + thirdLevel.cp)
+            nextRes.push(nextRes[1] + thirdLevel.res)
+            resCp.push(Math.round(nextRes[2] / nextCp[2]) + 1)
+        } else {
+            nextCp.push(nextCp[1])
+            nextRes.push(nextRes[1])
+            resCp.push(Math.round(nextRes[2] / nextCp[2]))
+        }
+
+        if(resCp[1] > 0 && resCp[0] <= resCp[1] && resCp[0] !== resCp[1]) {
+            building.to = building.level + 1
             building.nextCp = nextCp[0]
+            building.res = nextRes[0]
             building.resCp = resCp[0]
-        } else if(resCp[1] < resCp[2]) {
+        } else if(resCp[2] > 0 && resCp[1] <= resCp[2]) {
             building.to = building.level + 2
             building.nextCp = nextCp[1]
+            building.res = nextRes[1]
             building.resCp = resCp[1]
         } else {
             building.to = building.level + 3
             building.nextCp = nextCp[2]
+            building.res = nextRes[2]
             building.resCp = resCp[2]
         }
-
-        if(withSort) calculator.buildings.sort((a, b) => a.resCp - b.resCp)
     }
 
     const getResLevel = (buildingName, level) => {
-        const stats = buildingsData[buildingName][level]
+        try {
+            const stats = buildingsData[buildingName][level]
 
-        console.log(stats)
+            return {
+                res: stats.wood + stats.clay + stats.iron + stats.crop,
+                cp: stats.cp_diff
+            }
+        } catch (e) {
+            return {
+                res: 1,
+                cp: 0
+            }
+        }
+    }
 
-        return {
-            res: stats.wood + stats.clay + stats.iron + stats.crop,
-            cp: stats.cp_diff
+    const sortCPCost = () => {
+        calculator.buildings.sort((a, b) => a.resCp - b.resCp)
+    }
+
+    const sortBuildingName = () => {
+        calculator.buildings.sort((a, b) => {
+            if ( a.name < b.name ){
+                return -1;
+            }
+            if ( a.name > b.name ){
+                return 1;
+            }
+            return 0;
+        })
+    }
+
+    // tab 0    Res/CP
+    // tab 1    CP and Res
+    // tab 2    Free edit of levels
+    const swapTab = (key) => {
+        // swap away from current tab edit means recalculating
+        if(tab.value === 2)  calculateCPCost()
+
+        if(key === 1) {
+            if(tab.value === 0) tab.value = 1
+            else if(tab.value === 1) tab.value = 0
+            else tab.value = 0
+        }
+
+        if(key === 2) {
+            if(tab.value === 0 || tab.value === 1) tab.value = 2
+            else tab.value = 0
         }
     }
 
@@ -89,27 +148,93 @@
 <template>
     <CalculatorWrapper :title="calculator.title" @new:title="(t) => calculator.title = t" @close:calculator="close" type="village">
         <div class="px-2 w-full pb-3">
-            <table class="w-full">
+            <div class="flex gap-2 text-xs">
+
+                <button class="leading-none flex items-center justify-center gap-1 pb-1.5 pt-1 px-2 bg-slate-300 rounded cursor-pointer hover:bg-emerald-200" @click="swapTab(1)">
+                    <div>Data</div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-m-0.5 w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+                        </svg>
+
+                    </div>
+                </button>
+
+                <button class="leading-none flex items-center justify-center gap-1 pb-1.5 pt-1 px-2 bg-slate-300 rounded cursor-pointer hover:bg-emerald-200" @click="swapTab(2)">
+                    <div>Edit</div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-m-0.5 w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                        </svg>
+                    </div>
+                </button>
+
+            </div>
+            <table class="w-full mt-2">
                 <tr>
-                    <td class="text-center uppercase">Building</td>
-                    <td class="text-center uppercase">Level</td>
-                    <td class="text-center uppercase">To</td>
-                    <td class="text-center uppercase">CP</td>
-                    <td class="text-center uppercase pb-2">Res/CP</td>
-                </tr>
-                <tr v-for="building in calculator.buildings" class="border-b border-slate-400 odd:bg-slate-100">
-                    <td class="px-1.5 py-1">{{ building.name }}</td>
-                    <td class="">
-                        <div class="flex justify-between w-14">
-                            <div class="leading-none flex items-center justify-center font-bold pb-1 px-1.5 bg-slate-300 rounded cursor-pointer hover:bg-red-200" @click="modifyLevel(building, false)">-</div>
-                            <div>{{ building.level }}</div>
-                            <div class="leading-none flex items-center justify-center font-bold pb-0.5 px-1 bg-slate-300 rounded cursor-pointer hover:bg-emerald-200" @click="modifyLevel(building, true)">+</div>
+                    <td class="text-center uppercase pb-1">
+                        <div class="flex items-center justify-center gap-1 cursor-pointer" @click="sortBuildingName()">
+                            <div>Building</div>
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-m-0.5 w-4 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                                </svg>
+                            </div>
                         </div>
                     </td>
-                    <td class="px-1.5 text-right">{{ building.to ? building.to : building.level + 1}}</td>
-                    <td class="px-1.5 text-right">{{ building.nextCp ? building.nextCp : '...'}}</td>
-                    <td class="px-1.5 text-right">{{ building.resCp ? building.resCp : '...'}}</td>
+                    <td class="text-center uppercase pb-1">Level</td>
+                    <td class="text-center uppercase pb-1" v-if="tab === 0 || tab === 1">To</td>
+
+                    <template v-if="tab === 0">
+                        <td class="text-center uppercase pb-1">
+                            <div class="flex items-center justify-center gap-1 cursor-pointer" @click="sortCPCost()">
+                                <div>Res/CP</div>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="-m-0.5 w-4 h-4">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                                    </svg>
+                                </div>
+                            </div>
+                        </td>
+                    </template>
+
+                    <template v-if="tab === 1">
+                        <td class="text-center uppercase pb-1">Res</td>
+                        <td class="text-center uppercase pb-1">CP</td>
+                    </template>
                 </tr>
+
+                <tr v-for="building in calculator.buildings" class="border-b border-slate-400 odd:bg-slate-100">
+                    <td class="px-1.5 py-1">{{ building.name }}</td>
+
+                    <template v-if="tab === 0 || tab === 1">
+                        <td class="">
+                            <div class="flex justify-between w-14">
+                                <div class="leading-none flex items-center justify-center font-bold pb-1 px-1.5 bg-slate-300 rounded cursor-pointer hover:bg-red-200" @click="modifyLevel(building, false)">-</div>
+                                <div>{{ building.level }}</div>
+                                <div class="leading-none flex items-center justify-center font-bold pb-0.5 px-1 bg-slate-300 rounded cursor-pointer hover:bg-emerald-200" @click="modifyLevel(building, true)">+</div>
+                            </div>
+                        </td>
+                        <td class="px-1.5 text-right">{{ building.to ? building.to : building.level + 1}}</td>
+
+                        <template v-if="tab === 0">
+                            <td class="px-1.5 text-right">{{ building.resCp ? building.resCp : '...'}}</td>
+                        </template>
+
+                        <template v-if="tab === 1">
+                            <td class="text-right px-1.5">{{ building.res ? building.res : '...'}}</td>
+                            <td class="text-right px-1.5">{{ building.nextCp ? building.nextCp : '...'}}</td>
+                        </template>
+                    </template>
+
+                    <template v-if="tab === 2">
+                        <td class="text-right">
+                            <input type="number" class="py-0.5 px-1 border border-gray-300 w-14" v-model="building.level" min="0" :max="building.name === 'Cranny' ? 9 : 19">
+                        </td>
+                    </template>
+
+                </tr>
+
             </table>
         </div>
     </CalculatorWrapper>
--- src/components/calculators/Help.vue
+++ src/components/calculators/Help.vue
@@ -17,11 +17,16 @@
 <template>
     <CalculatorWrapper title="Calculator help" @close:calculator="close" :close="index >= 0">
         <div class="px-4 pt-1 pb-3 text-sm flex flex-col space-y-3">
-            <p>Use the icons on the left to open various calculators to assist in your Travian gameplay.</p>
+            <p><span class="font-semibold">Update 3.1</span> introduces a new CP helper and a confirmation on deleting all calculators.</p>
+            <p>Use the icons on the left to open calculators to assist in your Travian gameplay.</p>
             <p>You can open multiple tabs with calculators in desktop mode to distinguish between accounts or servers.</p>
-            <p>All data is stored locally on your browser, so it will be gone when you remove your cached data.</p>
-            <p>Update 3.0 introduces breaking changes, so all calculators were reset.</p>
+            <p>All data is stored locally on your browser, so it will be gone when you remove your local data.</p>
             <p>Questions or bugs? Add faerland on Discord</p>
+            <p class="text-xs pt-2 border-t border-slate-400">Known issues:</p>
+            <ul class="list-disc list-inside text-xs">
+                <li>Opening multiple of the same type of calculator and closing any closes the last one.</li>
+                <li>6c and 7c with 25/25 or 50% oasis shows wrong simulation for bakery. Simulate without oases to get the right numbers.</li>
+            </ul>
         </div>
     </CalculatorWrapper>
 </template>
